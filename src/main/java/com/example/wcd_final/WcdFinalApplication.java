2c995a8bbeb93aa60680d972a313423a249d0cf8
nguyenluu1203 <115715762+nguyenluu1203@users.noreply.github.com>
2024-12-24 09:08:19 +0700

    commit
    commit

---
+++ src/main/java/com/example/wcd_final/WcdFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.wcd_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcdFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcdFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wcd_final/WcdFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wcd_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcdFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
